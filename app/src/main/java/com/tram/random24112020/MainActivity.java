93c31bade5128ee439a0774b0af78d1677e317d5
tram421 <maitram0291@gmail.com>
2020-12-08 15:55:45 +0700

    My Layout in main 2

---
+++ app/src/main/java/com/tram/random24112020/MainActivity.java
@@ -0,0 +1,47 @@
+package com.tram.random24112020;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        //float a = 1.5f;
+        //double b=1.5;
+//        int a=10;
+//        String value = String.valueOf(a);
+//        Log.d("BBB",value);
+        //Lam tron
+//        float a= 1.5f;
+//        int result = Math.round(a);
+//        Log.d("BBB", String.valueOf(result));
+//        //Lam tron len
+//        double b= 1.5;
+//        double result_b = Math.round(b);
+//        Log.d("BBB_b", String.valueOf(result_b));
+//        //Lam tron xuong
+//        double c= 0.999999;
+//        double result_c = Math.floor(c);
+//        Log.d("BBB_b", String.valueOf(result_c));
+
+//        double result4=(Math.random())*10;
+//        double kq=result4%5;
+//        Log.d("BBB", String.valueOf(kq));
+
+//        double result4=Math.round(Math.random()*5);
+//        Log.d("BBB", String.valueOf(result4));
+//        Random random = new Random();
+//        int result5=random.nextInt(10);
+//        Log.d("BBB", String.valueOf(result5));
+//        Random random = new Random();
+//        int result5=random.nextInt(10-5+1)+5; //(max - min + 1) +min
+//        Log.d("BBB", String.valueOf(result5));
+    }
+}
